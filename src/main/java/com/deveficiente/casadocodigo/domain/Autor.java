94e6d8747436d7f178b2009585e08459d9df7a4d
ArthurPinheiro <arthurpinheirodelima@gmail.com>
2020-01-25 00:16:36 -0300

    Remoção de métodos que não eram utilizados.

--- src/main/java/com/deveficiente/casadocodigo/domain/Autor.java
+++ src/main/java/com/deveficiente/casadocodigo/domain/Autor.java
@@ -36,44 +36,5 @@ public class Autor {
         this.descricao = descricao;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public LocalDateTime getInstante() {
-        return instante;
-    }
-
-    public void setInstante(LocalDateTime instante) {
-        this.instante = instante;
-    }
 
 }
