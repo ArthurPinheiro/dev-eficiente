49de701a6017dffb41e7d4bcc18c66676f8a0c5c
ArthurPinheiro <arthurpinheirodelima@gmail.com>
2020-02-20 22:08:03 -0300

    Criação de estrutura para separar código de infra do controller.

--- src/main/java/com/deveficiente/casadocodigo/CasadocodigoApplication.java
+++ src/main/java/com/deveficiente/casadocodigo/CasadocodigoApplication.java
@@ -1,13 +1,25 @@
 package com.deveficiente.casadocodigo;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class CasadocodigoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CasadocodigoApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (factory) -> factory
+                .addContextCustomizers((context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
     }
 
 }
